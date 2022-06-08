dea4fcbdf74221b57cd8376a68af30cee35f9dad
Scott Stone <scott.allan.stone@gmail.com>
2022-06-08 16:45:48 -0600

    changed toml

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,16 @@ export default function Home() {
       <main>
         <Header title="hey, my name is Scott." />
         <p className="description">
-        I`m a PhD candidate in the department of psychology at the University of Alberta, supervised by Craig Chapman and Anthony Singhal. Recently, I`ve been working with BioWare on some projects involving eyetracking.
+          This is a simple starter project for Next.js.
 
-        I am interested in multimodal data collection, particularly eyetracking and motion capture. I also dabble in reinforcement learning and (a very little bit of) EEG.
-
-        I also do consulting for eyetracking, motion capture, and any sort of software/hardware development for lab purposes (e.g. stimulus presentation, analysis). Feel free to reach out if you want to chat.
         </p>
       </main>
 
-      <Footer />
+      <header>
+        <a href="https://www.google.ca">Google Scholar</a>
+      </header>
+
     </div>
   )
+
 }
